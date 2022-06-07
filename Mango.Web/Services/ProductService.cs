eacfe741b917d6303714c48f4b4703b278082236
Minh Nguyen <minh@partie.com>
2022-06-06 22:23:01 -0700

    first commit

---
+++ Mango.Web/Services/ProductService.cs
@@ -0,0 +1,39 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Services.IServices;
+
+namespace Mango.Web.Services
+{
+    public class ProductService : BaseService, IProductService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public ProductService(IHttpClientFactory httpClientFactory) : base(httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public Task<T> CreateProductAsync<T>(ProductDto productDto)
+        {
+            return SendAsync<T>(new ApiRequest() { ApiType = SD.ApiType.POST, Data = productDto, Url = SD.ProductApiBase + "/api/products", AccessToken = "" });
+        }
+
+        public Task<T> DeleteProductAsync<T>(int id)
+        {
+            return  SendAsync<T>(new ApiRequest() { ApiType = SD.ApiType.DELETE, Data = id, Url = SD.ProductApiBase + "/api/products", AccessToken = "" });
+        }
+
+        public Task<T> GetAllProductsAsync<T>()
+        {
+            return SendAsync<T>(new ApiRequest() { ApiType = SD.ApiType.GET, Url = SD.ProductApiBase + "/api/products/", AccessToken = "" });
+        }
+
+        public Task<T> GetProductByIdAsync<T>(int id)
+        {
+            return SendAsync<T>(new ApiRequest() { ApiType = SD.ApiType.GET, Url = SD.ProductApiBase + "/api/products/" + id, AccessToken = "" });
+        }
+
+        public Task<T> UpdateProductAsync<T>(ProductDto productDto)
+        {
+            return SendAsync<T>(new ApiRequest() { ApiType = SD.ApiType.PUT, Data = productDto, Url = SD.ProductApiBase + "/api/products/", AccessToken = "" });
+        }
+    }
+}
